303800cd42ccb2bac8c1585371b755d5b30b61eb
mogmog-0110 <fujimiya0110@gmail.com>
2026-05-24 19:39:41 +0900

    マグロのHPを20に調整

--- Enemy.hpp
+++ Enemy.hpp
@@ -212,7 +212,7 @@ public:
 	int getAnimeDir() const { return animeDir_; }
 
 	static constexpr int IMAGE_NUM = 2;
-	static constexpr int BOSS_HP = 60;
+	static constexpr int BOSS_HP = 20;
 	static constexpr int BOSS_ID = -1;
 
 private:
